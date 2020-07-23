1b412044247f931ea5f43ea983c691fba952ccf1
EFdez <escarlafdez@gmail.com>
2020-07-23 11:37:43 +0200

    client router

--- client/src/components/pages/pruebarutas/subjects.js
+++ client/src/components/pages/pruebarutas/subjects.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 
+import SchoolHackApi from '../../../service/SchoolHackApi'
+
+
 class Subjects extends Component {
-    constructor() {
-        super()
-        this.state = {}
+    constructor(props) {
+        super(props)
+        this.state = {
+            subjects: undefined,
+        }
+        this.schoolHackApi = new SchoolHackApi()
+    }
+
+    componentDidMount = () => {
+        this.schoolHackApi
+            .getAllSubjects()
+            .then(response => { this.setState({ subjects: response.data }) })
+            .catch(err => console.log(err))
     }
 
     render() {
         return (
             <>
                 <h1>Subjects</h1>
+
+                {!this.state.subjects ? <p>CARGANDO</p> :
+                    <ul>
+                        {this.state.subjects.map(elm => <li key={elm._id} {...elm}>{elm.title}</li>)}
+                    </ul>
+                }
             </>
         )
     }
--- client/src/components/pages/pruebarutas/teachers.js
+++ client/src/components/pages/pruebarutas/teachers.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react'
 
-class Teacher extends Component {
-    constructor() {
-        super()
-        this.state = {}
+import SchoolHackApi from '../../../service/SchoolHackApi'
+
+
+class Teachers extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            teachers: undefined,
+        }
+        this.schoolHackApi = new SchoolHackApi()
+    }
+
+    componentDidMount = () => {
+        this.schoolHackApi
+            .getAllTeachers()
+            .then(response => { this.setState({ teachers: response.data }) })
+            .catch(err => console.log(err))
     }
 
     render() {
         return (
             <>
-                <h1>Teacher</h1>
+                <h1>Teachers</h1>
+
+                {!this.state.teachers ? <p>CARGANDO</p> :
+                    <ul>
+                        {this.state.teachers.map(elm => <li key={elm._id} {...elm}>{elm.name} {elm.lastname}</li>)}
+                    </ul>
+                }
             </>
         )
     }
 }
 
-export default Teacher
+export default Teachers
